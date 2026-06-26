95ea034a375b64a08dab35620ca2e37f8f5b2dcf
Marit Alke <info@marit-alke.de>
2026-06-26 16:05:41 +0000

    Fix: paymentDetails.source auf 'payment' korrigiert (Rechnungsversand)

--- netlify/functions/mollie-webhook.js
+++ netlify/functions/mollie-webhook.js
@@ -93,7 +93,7 @@ async function createSalesInvoice({ paymentId, firstName, lastName, email, compa
       }
     ],
     paymentDetails: {
-      source: "mollie",
+      source: "payment",
       paymentId
     },
     emailDetails: {
